736893cf2bbaff86502d1e46b75ec80795dcd67a
Jake <thejakethree989@gmail.com>
2022-05-03 01:21:10 -0500

    Html done, working on JS

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount = 1;
+let resetButton;
+
+// Check Guess Function
+function checkGuess() {
+  const userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = "Previous guesses";
+  }
+  guesses.textContent += userGuess + " ";
+
+  if (userGuess === randomNumber) {
+    //Correct guess
+    lastResult.textContent = "Congratulations! You got it right!";
+    lastResult.getElementsByClassName.backgroundColor = "green";
+    lowOrHi.textContent = " ";
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = "!!!GAME OVER!!!";
+    lowOrHi.textContent = " ";
+    setGameOver();
+  } else {
+    lastResult.textContent = "Wrong!";
+    lastResult.getElementsByClassName.backgroundColor = "red";
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = "Last guess was too low!";
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = "Last guess was too high!";
+    }
+  }
+
+  guessCount++;
+  guessField.value = "";
+  guessField.focus();
+}
