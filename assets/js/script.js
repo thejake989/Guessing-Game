cf0773e3badb33274449474c6bd1c41bed013392
Jake <thejakethree989@gmail.com>
2022-05-03 01:35:54 -0500

    reset game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,3 +53,24 @@ function setGameOver() {
   document.body.append(resetButton);
   resetButton.addEventListener("Click", resetGame);
 }
+
+//Reset Game Function
+function resetGame() {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll(".resultParas p");
+  for (const resetPara of resetParas) {
+    resetPara.textContent = "";
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = "";
+  guessField.focus();
+
+  lastResult.style.backgroundColor = "white";
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
